575978d01f5e89cfff31ea6d49b26e1d3cf8d8d6
kkuhn1994 <kevkin548@gmail.com>
2025-06-09 17:35:29 +0200

    Deploy Flutter Web App to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "206409094"
+    serviceWorkerVersion: "2667453529"
   }
 });
